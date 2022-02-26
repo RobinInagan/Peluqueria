7045ea6540807c2f4f0f754602ba86a1d6503d3c
Robin <rsinagano@correo.udistrital.edu.co>
2022-02-26 09:47:51 -0500

    Carpetas Iniciales

---
+++ Home.php
@@ -0,0 +1,33 @@
+<!doctype html>
+<html lang="es">
+
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <!-- Bootstrap CSS 
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    -->
+  <link rel="stylesheet" language="javascript" href="./bootstrap/css/bootstrap.min.css">
+
+  <!-- Sweet alert-->
+  <link rel="stylesheet" href="./sw/dist/sweetalert2.min.css">
+
+  <!-- Iconos -->
+  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+  <script type="text/javascript" language="javascript" src="js/funciones.js"></script>
+
+  <title>Galfersh Barber</title>
+  <link rel="icon" type="image/x-icon" href="./Images/Logo.jpg">
+</head>
+
+<body>
+
+  <script src="./jquery/jquery-3.6.0.min.js"></script>
+  <script src="./sw/dist/sweetalert2.all.min.js"></script>
+  <script src="./bootstrap/js/bootstrap.min.js"></script>
+
+</body>
+
+</html>
