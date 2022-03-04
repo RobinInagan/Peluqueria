6150958c7bd734e38c15d7d135001556fb827d52
Sergio Mejia <sdmejiac@correo.udistrital.edu.co>
2022-03-04 18:37:29 -0500

    Errores corregidos

--- Home.php
+++ Home.php
@@ -16,7 +16,7 @@
 
     <!-- Iconos -->
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-    <!-- <script type="text/javascript" language="javascript" src="js/funciones.js"></script> -->
+    <script type="text/javascript" language="javascript" src="js/funciones.js"></script>
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
@@ -49,111 +49,118 @@
                         <a class="nav-link" href="#">Contacto</a>
                 </ul>
                 <button type="button" class="btn btn-outline-success" data-bs-toggle="modal" data-bs-target="#Login">Acceder</button>
-                <div class="modal fade" id="Login" tabindex="-1" aria-labelledby="Login" aria-hidden="true">
-                    <div class="modal-dialog modal-dialog-centered">
-                        <div class="modal-content">
-                            <div class="modal-header">
-                                <h5 class="modal-title" id="Login">Iniciar Sesión</h5>
-                                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                            </div>
-                            <div class="modal-body">
-                                <form>
-                                    <div class="mb-3">
-                                        <label for="recipient-name" class="col-form-label">Usuario: </label>
-                                        <input type="text" class="form-control" id="user">
-                                    </div>
-                                    <div class="mb-3">
-                                        <label for="message-text" class="col-form-label">Contraseña:</label>
-                                        <input type="password" class="form-control" id="paswd">
-                                    </div>
-                                </form>
-                            </div>
-                            <div class="modal-footer centrar">
-                                <button type="button" class="btn btn btn-success ">Iniciar Sesión</button>
-                            </div>
-                            <div class="mb-3 centrar">
-                                <a href=" ">Olvido Su Contraseña?</a>
-                            </div>
-                            <div class="mb-3 centrar">
-                                <a>¿No tiene cuenta aún? Registrese!</a><br>
-                                <button type="button" class="btn btn-secondary" data-bs-target="#Registrar" data-bs-toggle="modal" data-bs-dismiss="modal">Registrarse</button>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="modal fade" id="Registrar" aria-hidden="true" aria-labelledby="Registrar" tabindex="-1">
-                    <div class="modal-dialog modal-dialog-centered">
-                        <div class="modal-content">
-                            <div class="modal-header">
-                                <h5 class="modal-title" id="Registrar">Registrarse</h5>
-                                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                            </div>
-                            <div class="modal-body">
-                                <form>
-                                    <div class="mb-3">
-                                        <label for="recipient-name" class="col-form-label">Cédula: </label>
-                                        <input type="text" class="form-control" id="id_c">
-                                    </div>
-                                    <div class="mb-3">
-                                        <label for="recipient-name" class="col-form-label">Nombre(s): </label>
-                                        <input type="text" class="form-control" id="nombre">
-                                    </div>
-                                    <div class="mb-3">
-                                        <label for="recipient-name" class="col-form-label">Apellidos: </label>
-                                        <input type="text" class="form-control" id="apel">
-                                    </div>
-                                    <div class="mb-3">
-                                        <label for="recipient-name" class="col-form-label">Correo: </label>
-                                        <input type="email" class="form-control" id="mail">
-                                    </div>
-                                    <div class="mb-3">
-                                        <label for="recipient-name" class="col-form-label">Teléfono: </label>
-                                        <input type="text" class="form-control" id="tel_u">
-                                    </div>
-                                    <div class="mb-3">
-                                        <label for="recipient-name" class="col-form-label">Dirección: </label>
-                                        <input type="text" class="form-control" id="dir">
-                                    </div>
-                                    <div class="mb-3">
-                                        <label for="recipient-name" class="col-form-label">Fecha de Nacimiento: </label>
-                                        <input type="date" class="form-control" id="fecha_n">
-                                    </div>
-                                    <div class="modal-footer">
-                                        <button class="btn btn-primary" data-bs-toggle="modal" data-bs-dismiss="modal">Registrarse</button>
-                                    </div>
-                            </div>
-                            </form>
-                        </div>
-                    </div>
-                </div>
+            </div>
+        </div>
     </nav>
-    <div class="container">
-        <div class="p-3 mb-2">
-            <div id="carouselExampleIndicators" class="carousel slide " data-bs-ride="carousel">
-                <div class="carousel-indicators">
-                    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-                    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
-                    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+
+
+    <div class="modal fade" id="Login" tabindex="-1" aria-labelledby="Login" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="Login">Iniciar Sesión</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
-                <div class="carousel-inner">
-                    <div class="carousel-item active">
-                        <img src="./Images/Local.jpeg" class="d-block w-100" alt="..." height="600">
-                    </div>
-                    <div class="carousel-item">
-                        <img src="./Images/Local2.jpeg" class="d-block w-100" alt="..." height="600">
-                    </div>
-                    <div class="carousel-item">
-                        <img src="./Images/Local3.jpeg" class="d-block w-100" alt="..." height="600">
-                    </div>
+                <div class="modal-body">
+                    <form>
+                        <div class="mb-3">
+                            <label for="recipient-name" class="col-form-label">Usuario: </label>
+                            <input type="text" class="form-control" id="user">
+                        </div>
+                        <div class="mb-3">
+                            <label for="message-text" class="col-form-label">Contraseña:</label>
+                            <input type="password" class="form-control" id="paswd">
+                        </div>
+                    </form>
                 </div>
-                <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
-                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                    <span class="visually-hidden">Previous</span>
-                </button>
-                <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
-                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                    <span class="visually-hidden">Next</span>
-                </button>
+                <div class="modal-footer centrar">
+                    <button type="button" class="btn btn btn-success ">Iniciar Sesión</button>
+                </div>
+                <div class="mb-3 centrar">
+                    <a href=" ">Olvido Su Contraseña?</a>
+                </div>
+                <div class="mb-3 centrar">
+                    <a>¿No tiene cuenta aún? Registrese!</a><br>
+                    <button type="button" class="btn btn-secondary" data-bs-target="#Registrar" data-bs-toggle="modal" data-bs-dismiss="modal">Registrarse</button>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="modal fade" id="Registrar" aria-hidden="true" aria-labelledby="Registrar" tabindex="-1">
+        <div class="modal-dialog modal-dialog-centered">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="Registrar">Registrarse</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <form>
+                        <div class="mb-3">
+                            <label for="recipient-name" class="col-form-label">Cédula: </label>
+                            <input type="text" class="form-control" id="id_c">
+                        </div>
+                        <div class="mb-3">
+                            <label for="recipient-name" class="col-form-label">Nombre(s): </label>
+                            <input type="text" class="form-control" id="user">
+                        </div>
+                        <div class="mb-3">
+                            <label for="recipient-name" class="col-form-label">Apellidos: </label>
+                            <input type="text" class="form-control" id="apel">
+                        </div>
+                        <div class="mb-3">
+                            <label for="recipient-name" class="col-form-label">Correo: </label>
+                            <input type="email" class="form-control" id="mail">
+                        </div>
+                        <div class="mb-3">
+                            <label for="recipient-name" class="col-form-label">Teléfono: </label>
+                            <input type="text" class="form-control" id="tel_u">
+                        </div>
+                        <div class="mb-3">
+                            <label for="recipient-name" class="col-form-label">Dirección: </label>
+                            <input type="text" class="form-control" id="dir">
+                        </div>
+                        <div class="mb-3">
+                            <label for="recipient-name" class="col-form-label">Fecha de Nacimiento: </label>
+                            <input type="date" class="form-control" id="fecha_n">
+                        </div>
+                        <div class="modal-footer">
+                            <button class="btn btn-primary" data-bs-toggle="modal" data-bs-dismiss="modal">Registrarse</button>
+                        </div>
+                </div>
+                </form>
+            </div>
+        </div>
+    </div>
+
+    <div class="posicion">
+        <div class="container">
+            <div class="p-3 mb-2 centrar">
+                <div id="carouselExampleIndicators" class="carousel slide tamanio centrar" data-bs-ride="carousel">
+                    <div class="carousel-indicators">
+                        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+                    </div>
+                    <div class="carousel-inner tamanio centrar">
+                        <div class="carousel-item active">
+                            <img src="./Images/Local.jpeg" class="d-block w-100" alt="..." height="600" width="">
+                        </div>
+                        <div class="carousel-item">
+                            <img src="./Images/Local2.jpeg" class="d-block w-100" alt="..." height="600" width="">
+                        </div>
+                        <div class="carousel-item">
+                            <img src="./Images/Local3.jpeg" class="d-block w-100" alt="..." height="600" width="">
+                        </div>
+                    </div>
+                    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                        <span class="visually-hidden">Previous</span>
+                    </button>
+                    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                        <span class="visually-hidden">Next</span>
+                    </button>
+                </div>
             </div>
         </div>
     </div>
@@ -202,14 +209,14 @@
     ?>
 
     </table>
-
     <footer class="bg bg-dark text-white">
+
         <div class="centrar">
             <address>
                 <h3>Galfersh Barber</h3>
-                <p> <span class="format"></span> Cra 33 #23-45 </p>
-                <p><span class=""></span>323424455</p>
-                <p><span class=""></span>galfershbarber@gmail.com</p>
+                <p> <span class="oi oi-home footer-address-icon"></span>Cra 34 #43-44</p>
+                <p><span class="oi oi-phone footer-address-icon"></span>34322123</p>
+                <p><span class="oi oi-inbox footer-address-icon"></span>galfersh@gmail.com</p>
             </address>
         </div>
     </footer>
